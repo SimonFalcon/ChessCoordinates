2b51c78325e48fb5d71568e7992ba365c2608363
SimonFalcon <simonui.sakalauskui@gmail.com>
2024-06-07 12:05:29 +0300

    User, Play and statistics route and link setup

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     return(
         <header className='flex items-center gap-5 p-4 justify-center bg-gray-200'>
         {<img src={logo} className='object-contain w-14'/>}
-        <span className='font-bold text-xl'>ChessCoordinates</span>
+        <Link className='font-bold text-xl' to='/'>ChessCoordinates</Link>
         <div className='flex gap-4 p-1 '>
-          <div className='hover:text-green-600 m-3 font-bold text-l '>Play</div>
-          <div className='hover:text-green-600 m-3 font-bold text-l'>Statistics</div>
-          <div className='hover:text-green-600 m-3 font-bold text-l'>User</div>
+          <Link className='hover:text-green-600 m-3 font-bold text-l'to={'/play'}>Play</Link>
+          <Link className='hover:text-green-600 m-3 font-bold text-l'to={'/statistics'}>Statistics</Link>
+          <Link className='hover:text-green-600 m-3 font-bold text-l'to={'/user'}>User</Link>
         </div>
         <div>
           <Link className='hover:text-green-600 m-3 font-bold text-l 'to={'/login'}>Login</Link>
