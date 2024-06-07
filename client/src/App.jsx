b730b7a8e3cca515c05dcc8a7ca981f88d0a1540
SimonFalcon <simonui.sakalauskui@gmail.com>
2024-06-07 11:41:45 +0300

    Index, login,, register routes set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css'
-import logo from './assets/logo.png';
+import {Route, Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage.jsx";
+
 function App() {
 
   return (
-    <div>
-      <header className='flex items-center gap-5 p-4 justify-center'>
-        <img src={logo} className='object-contain w-14'/>
-        <span className='font-bold text-xl'>ChessCoordinates</span>
-        <div className='flex gap-4 p-1 '>
-          <div className='hover:text-green-600 m-3 font-bold text-l '>Play</div>
-          <div className='hover:text-green-600 m-3 font-bold text-l'>Statistics</div>
-          <div className='hover:text-green-600 m-3 font-bold text-l'>User</div>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Route>
+    </Routes>
+    
   )
 }
 
